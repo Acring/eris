c62f9bb7a738aaa9d5a13c464c80b1df6bf16670
acring <liu-zhen@xsky.com>
2025-08-28 10:27:17 +0800

    feat: 增加 docs 描述
    Signed-off-by: acring <liu-zhen@xsky.com>

---
+++ apps/storybook/scripts/extract-storybook-docs.sh
@@ -0,0 +1,5 @@
+../../node_modules/.bin/storybook-llms-extractor \
+  --distPath storybook-static \
+  --summaryBaseUrl https://acring.github.io/eris \
+  --summaryTitle "Eris 组件库" \
+  --summaryDescription "Eris 组件库是基于 React 和 Tailwind CSS 构建的组件库，提供了丰富的组件和样式，用于构建现代化的 Web 应用程序。"
